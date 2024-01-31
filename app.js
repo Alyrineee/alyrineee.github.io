f3867cef5201153c129271f3a2236c9a37ed6dec
Alyrineee <148715036+Alyrineee@users.noreply.github.com>
2024-01-31 22:08:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 window.addEventListener('resize', () => {
-	sizes.width  = window.innerWidth/3;
+	sizes.width  = window.innerWidth;
 	sizes.height  = window.innerHeight;
 	camera.aspect = sizes.width/sizes.height;
 	camera.updateProjectionMatrix();
